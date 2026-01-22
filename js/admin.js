34773139fb201f3df91b102a90c827ab9b4d72f6
junhyunsim0402 <simjjang11@gmail.com>
2026-01-22 14:40:23 +0900

    합체

--- js/admin.js
+++ js/admin.js
@@ -1,14 +1,15 @@
 AutoDangerIdentify();
 function AutoDangerIdentify(){
-    const DangerList = JSON.parse(localStorage.getItem(`DangerList`));
-    const userContent=document.querySelector(`.usercontent`);
-    const userPost=userContent.querySelector(`td`);
-    const userdelete=userContent.querySelector(`tr`);
-    localStorage.getItem(`DangerList`);
+    const DangerList = JSON.parse(localStorage.getItem(`DangerList`));//위험키워드 호출하여 변수에 저장
+    const userContent=document.querySelector(`.usercontents`);//리스트 가져오기
+    const userdelete=userContent.querySelector(`tr`);//삭제할 줄
+    
+    localStorage.getItem(`DangerList`);//위험키워드 호출
+    
     for(let i=0;i<DangerList.length;i++){
         const dangerlist=DangerList[i];//금지 키워드 저장
-        if(userPost.textContent.includes(DangerList[i])){//금지 키워드 찾기
-            userPost.innerHTML=userPost.innerHTML.replace(dangerlist,`<span style="color:red;">${dangerlist}</span>`);//키워드를 찾으면 색 교체
+        if(userContent.textContent.includes(DangerList[i])){//금지 키워드 찾기
+            userContent.innerHTML=userContent.innerHTML.replace(dangerlist,`<span style="color:red;">${dangerlist}</span>`);//키워드를 찾으면 색 교체
             setTimeout(function(){
                 alert(`금지키워드:${DangerList[i]}발견`);
                 userdelete.remove();
@@ -20,8 +21,8 @@ function AutoDangerIdentify(){
 AutoEmphasizeIdentify();
 function AutoEmphasizeIdentify(){
     const EmphasizeList = JSON.parse(localStorage.getItem(`EmphasizeList`));
-    const userContent=document.querySelector(`.usercontent`);
-    const userPost=userContent.querySelector(`td`);
+    const userContents=document.querySelector(`.usercontents`);
+    const userPost=userContents.querySelector(`td`);
     localStorage.getItem(`EmphasizeList`);
     for(let i=0;i<EmphasizeList.length;i++){
         const emphasizelist=EmphasizeList[i];//강조 키워드 저장
